239bbd179824101eb95e56228ec063f65085fac1
Billy Farroll <watanight@hotmail.co.uk>
2015-10-22 09:34:32 +0100

    Create viewController.swift

---
+++ viewController.swift
@@ -0,0 +1,38 @@
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController  {
+
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+    
+    
+    }
+    
+    
+
+}
+
+extension ViewController: CLLocationManagerDelegate {
+
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        
+        
+        //Something went wrong...
+    }
+
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let newLocations = locations.last
+        print(newLocations)
+    }
+    
+    
+}
